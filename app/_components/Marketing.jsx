710e48d627b831e859b6feed1d68de8a06329aa6
Gumilarmn <Gumilarmn16@gmail.com>
2024-06-27 10:09:58 +0700

    add button styling

--- app/_components/Marketing.jsx
+++ app/_components/Marketing.jsx
@@ -8,7 +8,7 @@ function Marketing() {
         <div className='flex flex-row bg-black mt-10'>
             <div className='flex flex-col items-center justify-center ml-auto'>
                 <h1 className='text-white text-center text-2xl'>Experience the Best in Beauty <br /> Book Your Appointment at SEA Salon</h1>
-                <Link href="/dashboard"><Button variant='outline' className='2xl:mt-64 bg-transparent text-white mt-5'>Book Now</Button></Link>
+                <Link href="/dashboard"><Button className='2xl:mt-64 bg-transparent border-2 border-white text-white mt-5 hover:border-2 hover:bg-white hover:text-black hover:border-white'>Book Now</Button></Link>
             </div>
             <div className='ml-auto'>
                 <img src="/salonenv.jpeg" alt="salon Env" />
--- app/_components/Service.jsx
+++ app/_components/Service.jsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { MoveRight } from 'lucide'
+import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
 function Service() {
   return (
-    <div className='my-24'>
+    <div className=''>
       <h1 className='text-5xl text-center md:mt-10'>Our Services</h1>
       <p className='text-gray-600 text-center text-sm md:mt-4 2xl:mt-6'>Indulge in our top-tier services: expert haircuts and styling, meticulous manicure and pedicure, <br/> and rejuvenating facial treatments.</p>
 
@@ -25,6 +28,10 @@ function Service() {
           <h2 className='text-2xl px-5 pt-5 max-w-full text-center'>Facial Treatments</h2>
           <p className='text-gray-600 text-sm px-5 pb-5 max-w-full text-center'>Revitalizing treatments for <br />radiant skin</p>
         </div>
+      </div>
+      <div className='flex justify-center pt-10'>
+      <Button className='items-center bg-black hover:bg-transparent hover:border-2 hover:text-black hover:border-black transition-all'>
+        More Services <ArrowRight className='ml-2'/></Button>
       </div>
     </div>
   )
